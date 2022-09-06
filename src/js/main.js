4c13fd3baaa93423581b7069f0b5da475857fd72
Jesus Herrera <heisusin@gmail.com>
2022-09-06 18:54:35 +0200

    feat: ✨ Added actors data and colors style

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ import ora from 'ora';
 import chalk from 'chalk';
 
 const apiKey = process.env.API_KEY;
-import https from 'https'
+import https from 'https';
 
 const program = new Command();
 
@@ -27,20 +27,12 @@ program
 		'The page of persons data results to fetch'
 	)
 	.action((load) => {
-		const spinner = ora(
-			`Loading ${chalk.bgCyan('Most popular persons')}`
-		).start();
-		setTimeout(() => {
-			spinner.color = 'white';
-			spinner.text = 'Loading rainbows';
-			spinner.succeed('Succeed');
-		}, 2000);
 
-		// const page = 2;
+		const spinner = ora(`Loading ${chalk.bgCyan('Most popular persons')}`).start();
 
-		https.get(
-			`https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${load.page}`,
-			(resp) => {
+		const urlApi = `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${load.page}`;
+
+		https.get(urlApi, (resp) => {
 				let data = '';
 
 				// Data fragment has been received
@@ -49,13 +41,52 @@ program
 				});
 
 				resp.on('end', () => {
-					console.log(JSON.parse(data));
-				});
-			}
-		)
-    .on('error', (error) => {
-      console.log('error: ' + error.message);
-    })
+					const dataJSON = JSON.parse(data)
+					const pagination = dataJSON.total_pages;
+					console.log(JSON.parse(data))
+					
+          setTimeout(() => {
+            // spinner.succeed('Success')
+
+						dataJSON.results.map(({id, name, known_for_department, known_for}) => {
+							console.log(chalk.white('----------------------------------------'));
+							console.log(chalk.white('Person:'));
+							console.log('\n');
+							console.log('Id:', chalk.yellowBright(id));
+							console.log('Name:', chalk.cyanBright(name));
+							known_for_department === 'Acting'
+							? console.log('Department:', chalk.magentaBright(known_for_department))
+							: '';
+							
+							if (known_for.length > 0) {
+								known_for.map(({id, release_date, title}) => {
+									if (title !== undefined) {
+										console.log('\n')
+										console.log('\t Movie:')
+										console.log('\t Id:', chalk.greenBright(id));
+										console.log('\t Release date:', chalk.blueBright(release_date));
+										console.log('\t Title:', chalk.redBright.bold(title));
+										console.log('\n');
+									} else {
+										console.log('\n','\t', chalk.yellow(`${name} doesn't appear in any movie`), '\n')
+									}
+								})
+							}
+						})
+						spinner.succeed('Success')
+
+						console.log(chalk.white(`
+----------------------------------------
+Page: ${load.page} of: ${pagination}
+`));
+							}, 1000);
+						});
+					})
+
+			.on('error', (error) => {
+				console.log('error: ' + error.message);
+        spinner.fail('Request failed')
+			});
 	});
 
 program.parse(process.argv);
